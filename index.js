17b0c3fed1fb129fb5975d4213ee9d4f495479f9
Farid Ibrahim <fareedibrahim0003@gmail.com>
2022-06-06 09:37:21 +0100

    fixed display

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 function openNav(){document.getElementById("mySidenav").style.width="250px";$("body").css("background-color","#d1d1d1")}
 function closeNav(){document.getElementById("mySidenav").style.width="0";$("body").css("background-color","inherit")}
-function myFunction(x){if(x.matches){$(".phone").removeClass("phone-div")}else{console.log("javascript media query failed")}}
-var x=window.matchMedia("(max-width: 800px)")
-myFunction(x)x.addListener(myFunction)
